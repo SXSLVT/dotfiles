0c0826f9b642272fb1f5da74a58591dd84df46df
Syner <ploscarusynxy@gmail.com>
2026-05-04 15:21:43 +0000

    Add files via upload

--- waybar/wallpaper-picker.sh
+++ waybar/wallpaper-picker.sh
@@ -2,17 +2,23 @@
 
 WALLPAPERS_DIR="$HOME/Wallpapers"
 
-find "$WALLPAPERS_DIR" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" \) | while read -r img; do
+selected=$(find "$WALLPAPERS_DIR" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" \) | while read -r img; do
     name=$(basename "$img")
     echo -e "img:$img:text:$name"
 done | \
 wofi --dmenu \
      --prompt "Select Wallpaper..." \
      --width 500 \
-     --height 250 \
+     --height 230 \
      --cache-file /dev/null \
      --normal-window \
      --insensitive \
      --allow-images \
-     --image-size 100 | \
-     sed 's/.*:text://' | xargs -I {} find "$WALLPAPERS_DIR" -name "{}" | xargs -I {} ~/.config/waybar/wal-set "{}"
+     --image-size 150)
+
+if [ -n "$selected" ]; then
+    img_path=$(echo "$selected" | sed 's/^img://;s/:text:.*$//')
+    cp "$img_path" "$HOME/.cache/current_wallpaper.png"
+    awww img "$img_path" --transition-type right
+    ~/.config/waybar/wal-set "$img_path"
+fi
