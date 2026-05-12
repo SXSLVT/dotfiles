64b19886dbbac16be8c4fd6e30350310381e6997
Syner <ploscarusynxy@gmail.com>
2026-05-12 19:30:17 +0000

    Delete waybar directory

--- waybar/scripts/toggle-weather-unit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-UNIT_FILE="$HOME/.cache/weather-unit"
-[ ! -f "$UNIT_FILE" ] && echo "C" > "$UNIT_FILE"
-
-CURRENT=$(cat "$UNIT_FILE")
-[ "$CURRENT" = "C" ] && echo "F" > "$UNIT_FILE" || echo "C" > "$UNIT_FILE"
-
-pkill -SIGRTMIN+8 waybar
